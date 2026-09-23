e6a42f37b4a181973968c543c307ba24198c6679
Kevin Vranken <29517264+Helaas@users.noreply.github.com>
2026-09-23 22:05:43 +0200

    Start Weston from / rather than the launcher's card directory

--- internal/platform/device_mlp1.c
+++ internal/platform/device_mlp1.c
@@ -1434,7 +1434,7 @@ static int jw__mlp1_set_refresh_rate(int hz) {
                 dup2(devnull, STDERR_FILENO);
             }
             execl("/bin/sh", "sh", "-c",
-                  "/etc/init.d/S49weston restart; sleep 1; "
+                  "cd / && /etc/init.d/S49weston restart; sleep 1; "
                   "kill -9 $(pgrep -x jawaka-launcher) $(pgrep -x jawaka-osd) 2>/dev/null",
                   (char *)NULL);
             _exit(127);
@@ -1492,7 +1492,7 @@ static int jw__mlp1_set_hdmi_output(int mode) {
         snprintf(script, sizeof(script),
             "rm -f /tmp/.weston_drm.conf %s; "
             "export WESTON_DRM_SINGLE_HEAD=1 WESTON_DRM_PRIMARY=DSI-1; "
-            "/etc/init.d/S49weston restart; sleep 1; "
+            "cd / && /etc/init.d/S49weston restart; sleep 1; "
             "kill -9 $(pgrep -x jawaka-launcher) $(pgrep -x jawaka-osd) 2>/dev/null",
             JW_MLP1_WESTON_OVERRIDE_INI);
     } else {
@@ -1510,7 +1510,7 @@ static int jw__mlp1_set_hdmi_output(int mode) {
             "rm -f %s; "
             "printf 'output:HDMI-A-1:mode=%s\\n%soutput:HDMI-A-1:primary\\n' > /tmp/.weston_drm.conf; "
             "export WESTON_DRM_SINGLE_HEAD=1 WESTON_DRM_PRIMARY=HDMI-A-1 WESTON_DRM_VIRTUAL_SIZE=960x720 WESTON_DRM_CONFIG=/tmp/.weston_drm.conf; "
-            "/etc/init.d/S49weston restart; sleep 1; "
+            "cd / && /etc/init.d/S49weston restart; sleep 1; "
             "kill -9 $(pgrep -x jawaka-launcher) $(pgrep -x jawaka-osd) 2>/dev/null",
             JW_MLP1_WESTON_OVERRIDE_INI, modestr, rectline);
     }
